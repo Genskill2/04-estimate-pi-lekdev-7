40d56bc71553b29cf69e314c98a89dbe1dbcd429
lekdev-7 <71559815+lekdev-7@users.noreply.github.com>
2021-06-03 22:46:28 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -2,59 +2,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#define random rand
-#define srandom srand
 
-
-float mc_pi(int);
-
-float frandom() {
-  long int q = random();
-  float ret = (float)q/(float)RAND_MAX;
-  return ret;
-}
+float wallis_pi(int);
 
 int main(void) {
-  float pi0;
-  float pi1;
-  
-  pi0 = mc_pi(25000);
-  pi1 = mc_pi(25000);
-  printf("%f %f\n", pi0, pi1);
-  
-  if (pi0 == pi1) {
-      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
+  float pi;
+  for (int i=0; i<5; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) > 0.15)) {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
     }
-
-  if (fabs(pi0 - pi1) > 0.05) {
-      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
-      abort();
   }
 
-    
-  for (int i=2000; i<5000; i++) {
-    pi0 = mc_pi(i);
-    if (!(fabs(pi0 - M_PI) < 0.4)) {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
+  for (int i=500; i<3000; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
       abort();
     }
   }
 }
-
-float mc_pi(int n)
-{  float pi,x,y,z;
-int count=0;
-    for(int i = 0; i < n; ++i) {
-
-     x = frandom();
-
-     y = frandom();
-
-     if((x * x + y * y)<= 1 ) count++;
- }
-
- pi =  count /( n * 4);
-
- return (float)pi;
-}
+  
+  float wallis_pi(int x)
+  { float y;
+      y = 1.0000 ;
+   
+   
+     for( int n=1; n<=x ; n++)
+     { 
+         y = y*(4.0*n*n)/((4.0*n*n)-1);
+       
+      }
+   y = y*2;
+   return y;
+  }
